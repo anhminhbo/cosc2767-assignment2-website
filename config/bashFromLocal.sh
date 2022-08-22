de6135cc5c17298434dbf8c96d5aa2831b50f5f6
Minh Nguyens <wormscott12397@gmail.com>
2022-08-23 02:06:58 +0700

    update some bash script

---
+++ config/bashFromLocal.sh
@@ -0,0 +1,1 @@
+scp -r ./jenkins/* jenkinsadmin@ec2-18-215-182-164.compute-1.amazonaws.com:
---
+++ config/jenkins/bashInJenkinsServer.sh
@@ -0,0 +1,25 @@
+# Start docker service
+sudo service docker start
+# Clean up docker container and image
+sudo docker stop jenkins-container;
+sudo docker rm jenkins-container;
+sudo docker rmi --force jenkins_asm2_3931605:latest
+sudo docker rmi --force anhminhbo/jenkins_asm2_3931605:latest
+sudo docker rmi --force jenkins/jenkins:latest
+
+# Log out and then log in to push image to DockerHub
+sudo docker logout
+sudo docker login -u "anhminhbo" -p "Anhminh1234*" docker.io
+sudo docker build -t jenkins_asm2_3931605:latest .
+sudo docker tag tomcat_asm2_3931605:latest anhminhbo/tomcat_asm2_3931605:latest
+sudo docker push anhminhbo/jenkins_asm2_3931605:latest
+
+# Clean up Dockerfile and current image
+sudo docker rmi --force anhminhbo/jenkins_asm2_3931605:latest
+sudo rm -rf Dockerfile
+
+# Run the container pull from DockerHub
+docker run -d --name jenkins-container --rm -p 8080:8080 anhminhbo/jenkins_asm2_3931605:latest
+
+# Go inside the jenkins container
+docker exec -it jenkins-container /bin/bash
