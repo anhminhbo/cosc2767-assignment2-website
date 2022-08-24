82431a7818ecca6d8e0fa9480055cedbdb0e0246
Minh Nguyens <wormscott12397@gmail.com>
2022-08-24 15:13:59 +0700

    update

--- config/PushFilesToJenkinsServer.sh
+++ config/PushFilesToJenkinsServer.sh
@@ -10,5 +10,12 @@
 
 # Change the public Id because ec2 automatically switch to new public Id
 scp -o PubkeyAuthentication=no -r ./jenkins/* jenkinsmaster@:
+
 # To ssh to a remote server without Publickey
 # ssh -o PubkeyAuthentication=no
+
+# To ssh to a remote server with the right public key
+# ssh -o 'IdentitiesOnly yes' -i "asm2.pem"
+
+# Set remote server using access token for RMIT github account
+# git remote set-url origin https://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@github.com/MintCici1007/cosc2767-assignment2-website.git  
